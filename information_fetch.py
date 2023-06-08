744d082860213b330ca079b04b73ed635704492f
Alex Tjellesen <alex.tjellesen@gmail.com>
2023-06-08 16:30:24 -0500

    modified to fetch go terms

--- information_fetch.py
+++ information_fetch.py
@@ -1,10 +1,26 @@
 from Bio import SeqIO
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
+import requests
+
+def get_gene_ontology_annotations(protein_id):
+    url = f"https://www.ebi.ac.uk/QuickGO/services/annotation/search?geneProductId={protein_id}"
+    headers = {"Accept": "application/json"}
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        annotations = data["results"]
+        return annotations
+    else:
+        print(f"Failed to retrieve gene ontology annotations for protein {protein_id}.")
+        return []
+
+fasta_file = "uniprot-download_true_format_fasta_query__28_28proteome_3AUP00005856-2023.06.08-20.05.13.50.fasta"
 
 # count = 0
-
 proteins = {}
-for record in SeqIO.parse("GCF_001534645.1_ASM153464v1_protein.faa", "fasta"):
+for record in SeqIO.parse(fasta_file, "fasta"):
+    id = record.id.split("|")[1].strip()
     seq = record.seq
     a_seq = ProteinAnalysis(seq)
 
@@ -19,11 +35,13 @@ for record in SeqIO.parse("GCF_001534645.1_ASM153464v1_protein.faa", "fasta"):
         "aromaticity": a_seq.aromaticity(),
         "flexibility": a_seq.flexibility(),
         "sec_sruct_frac": a_seq.secondary_structure_fraction(),
+        "go_annotations": get_gene_ontology_annotations(id)
     }
+
+    proteins[id] = protein
+
     # count+=1
     # print(count)
-
-    proteins[record.id] = protein
 
 for protein_id, protein_info in proteins.items():
     print(f"Protein ID: {protein_id}")
@@ -37,4 +55,9 @@ for protein_id, protein_info in proteins.items():
     print(f"Aromaticity: {protein_info['aromaticity']}")
     print(f"Flexibility: {protein_info['flexibility']}")
     print(f"Secondary Structure Fraction: {protein_info['sec_sruct_frac']}")
+    # can also access other GO information such as goEvidence
+    for annotation in protein_info['go_annotations']:
+        go_term = annotation["goId"]
+        go_aspect = annotation["goAspect"]
+        print(f"Gene Ontology term: {go_term} ({go_aspect})")
     print()
