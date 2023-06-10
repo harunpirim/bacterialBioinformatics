1578ad79395737f48332a0042698a5e2de095ef1
Alex Tjellesen <alex.tjellesen@gmail.com>
2023-06-10 13:27:38 -0500

    go term searching from json instead of web

--- information_fetch.py
+++ information_fetch.py
@@ -1,26 +1,29 @@
+import json
 from Bio import SeqIO
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
-import requests
 
-def get_gene_ontology_annotations(protein_id):
-    url = f"https://www.ebi.ac.uk/QuickGO/services/annotation/search?geneProductId={protein_id}"
-    headers = {"Accept": "application/json"}
-
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        annotations = data["results"]
-        return annotations
-    else:
-        print(f"Failed to retrieve gene ontology annotations for protein {protein_id}.")
-        return []
+def get_go_ids(protein_id, results):
+    go_ids = []
+    for result in results:
+        if result["primaryAccession"] == protein_id:
+            references = result["uniProtKBCrossReferences"]
+            for reference in references:
+                if reference["database"] == "GO":
+                    go_id = reference["id"]
+                    go_ids.append(go_id)
+    return go_ids
 
 fasta_file = "uniprot-download_true_format_fasta_query__28_28proteome_3AUP00005856-2023.06.08-20.05.13.50.fasta"
+json_file = "uniprot-download_true_format_json_query__28_28proteome_3AUP000058566-2023.06.08-21.46.26.53.json"
 
-# count = 0
+# Load the JSON data
+with open(json_file) as file:
+    data = json.load(file)
+    results = data["results"]
+
 proteins = {}
 for record in SeqIO.parse(fasta_file, "fasta"):
-    id = record.id.split("|")[1].strip()
+    protein_id = record.id.split("|")[1].strip()
     seq = record.seq
     a_seq = ProteinAnalysis(seq)
 
@@ -35,13 +38,10 @@ for record in SeqIO.parse(fasta_file, "fasta"):
         "aromaticity": a_seq.aromaticity(),
         "flexibility": a_seq.flexibility(),
         "sec_sruct_frac": a_seq.secondary_structure_fraction(),
-        "go_annotations": get_gene_ontology_annotations(id)
+        "go_ids": get_go_ids(protein_id, results)
     }
 
-    proteins[id] = protein
-
-    # count+=1
-    # print(count)
+    proteins[protein_id] = protein
 
 for protein_id, protein_info in proteins.items():
     print(f"Protein ID: {protein_id}")
@@ -55,9 +55,6 @@ for protein_id, protein_info in proteins.items():
     print(f"Aromaticity: {protein_info['aromaticity']}")
     print(f"Flexibility: {protein_info['flexibility']}")
     print(f"Secondary Structure Fraction: {protein_info['sec_sruct_frac']}")
-    # can also access other GO information such as goEvidence
-    for annotation in protein_info['go_annotations']:
-        go_term = annotation["goId"]
-        go_aspect = annotation["goAspect"]
-        print(f"Gene Ontology term: {go_term} ({go_aspect})")
+    for go_id in protein_info['go_ids']:
+        print(f"Gene Ontology IDs: {go_id}")
     print()
