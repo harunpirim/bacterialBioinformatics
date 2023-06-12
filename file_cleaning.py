121f1e23efb606b2f861acaeb988f522df903137
Alex Tjellesen <alex.tjellesen@gmail.com>
2023-06-12 00:58:40 -0500

    removed data from git and cleaned code

---
+++ file_cleaning.py
@@ -0,0 +1,67 @@
+import csv
+import json
+from Bio import SeqIO
+
+
+def nested_dict_to_json(nested_dict, file_path):
+    with open(file_path, 'w') as json_file:
+        json.dump(nested_dict, json_file, indent=4)
+
+def json_to_nested_dict(file_path):
+    with open(file_path, 'r') as json_file:
+        nested_dict = json.load(json_file)
+    return nested_dict
+
+#--- iFeature cleaning ---
+def create_dictionary_from_iFeature_tsv(ifeature_file):
+    dictionary = {}
+    with open(ifeature_file, 'r') as file:
+        reader = csv.reader(file, delimiter='\t')
+        headers = next(reader) 
+        for row in reader:
+            key = row[0].split("|")[1].strip()  #grabs only protein id
+            values = {header: value for header, value in zip(headers[1:], row[1:])}
+            dictionary[key] = values
+    return dictionary
+
+ifeature_files = {
+"dpc" : "data\\dpc.tsv",
+"tpc" : "data\\tpc.tsv",
+"paac" : "data\\paac.tsv",
+"cdtc" : "data\\cdtc.tsv",
+"cdtd" : "data\\cdtd.tsv",
+"cdtt" : "data\\cdtt.tsv",
+"ctriad" : "data\\ctriad.tsv",
+"gaac" : "data\\gaac.tsv",
+"moran" : "data\\moran.tsv"
+}
+
+combined_dictionary = {}
+
+for key, value in ifeature_files.items():
+    combined_dictionary[key] = create_dictionary_from_iFeature_tsv(value)
+
+# nested_dict_to_json(combined_dictionary, "iFeature.json")
+
+#--- Prosite cleaning ---
+motif_file = "data\\motifs_wo_profiles.fasta"
+
+motifs = {}
+for record in SeqIO.parse(motif_file, "fasta"):
+    id = record.id.split("|")[1].strip()
+    seq = str(record.seq)
+    if id not in motifs:
+        motifs[id] = [seq]
+    else:
+        motifs[id].append(seq)
+
+# nested_dict_to_json(motifs, "Prosite.json")
+
+#--- UniProt cleaning ---
+uniprot_file = "raw_data\\uniprot-download_true_format_json_query__28_28proteome_3AUP000058566-2023.06.08-21.46.26.53.json"
+
+dict = json_to_nested_dict(uniprot_file)
+# nested_dict_to_json(dict, "data\\UniProt.json")
+
+
+
--- information_fetch.py
+++ information_fetch.py
@@ -2,115 +2,89 @@ import json
 from Bio import SeqIO
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
 
-def create_dictionary_from_tsv(tsv_file):
-    dictionary = {}
+# -- Variables: Protein Dictionary ---
+proteins = {}
 
-    with open(tsv_file, 'r') as file:
-        reader = csv.reader(file, delimiter='\t')
+#--- Variables: File Locations ---
+protein_file = "data\\proteins-2023.06.08-20.05.13.50.fasta"
+uniprot_file = "data\\UniProt.json"
+motif_file = "data\\Prosite_wo.json"
+ifeature_file = "data\\iFeature.json"
 
-        headers = next(reader) 
-
-        for row in reader:
-            key = row[0].split("|")[1].strip()  #grabs only protein id
-            values = {header: value for header, value in zip(headers[1:], row[1:])}
-            dictionary[key] = values
-
-    return dictionary
-
-def get_go_terms(protein_id, results):
-    go_terms = []
+#--- Functions: Assign Annotations (UniProt) ---
+def assign_annotations():
+    # Load JSON data from UniProt file
+    with open(uniprot_file, 'r') as file:
+        data = json.load(file)
+        results = data["results"]
+    
+    global proteins
     for result in results:
-        if result["primaryAccession"] == protein_id:
-            if "uniProtKBCrossReferences" in result:
-                references = result["uniProtKBCrossReferences"]
-                for reference in references:
-                    if reference["database"] == "GO":
-                        go_id = reference["id"]
-                        go_term = reference["properties"][0]["value"]
-                        go_terms.append((go_id, go_term))
+        id = result["primaryAccession"]
+        if "uniProtKBCrossReferences" in result: proteins[id]["go_terms"] = get_go_terms(result)
+        if "comments" in result: proteins[id]["subcell_locations"] = get_subcellular_locations(result)
+        if "features" in result: proteins[id]["transmembrane"] = get_transmembrane(result)
+        if "keywords" in result: proteins[id]["binding_preference"] = get_binding_preference(result)
+
+def get_go_terms(protein_info):
+    go_terms = []
+    references = protein_info["uniProtKBCrossReferences"]
+    for reference in references:
+        if reference["database"] == "GO":
+            go_id = reference["id"]
+            go_term = reference["properties"][0]["value"]
+            go_terms.append((go_id, go_term))
     return go_terms
 
-def get_subcellular_locations(protein_id, results):
+def get_subcellular_locations(protein_info):
     subcell_locations = []
-    for result in results:
-        if result["primaryAccession"] == protein_id:
-            if "comments" in result:
-                comments = result["comments"]
-                for comment in comments:
-                    if comment["commentType"] == "SUBCELLULAR LOCATION":
-                        locations = comment["subcellularLocations"]
-                        for location in locations:
-                            subcell_locations.append(location["location"]["value"])
+    comments = protein_info["comments"]
+    for comment in comments:
+        if comment["commentType"] == "SUBCELLULAR LOCATION":
+            locations = comment["subcellularLocations"]
+            for location in locations:
+                subcell_locations.append(location["location"]["value"])
     return subcell_locations
 
-def get_transmembrane(protein_id, results):
+def get_transmembrane(protein_info):
     transmembrane = 0
-    for result in results:
-        if result["primaryAccession"] == protein_id:
-            if "features" in result:
-                features = result["features"]
-                for feature in features:
-                    if feature["type"] == "Transmembrane":
-                        transmembrane = 1
+    features = protein_info["features"]
+    for feature in features:
+        if feature["type"] == "Transmembrane":
+            transmembrane = 1
     return transmembrane
 
-def get_binding_preference(protein_id, results):
+def get_binding_preference(protein_info):
     bp = []
-    for result in results:
-        if result["primaryAccession"] == protein_id:
-            if "keywords" in result:
-                keywords = result["keywords"]
-                for keyword in keywords:
-                    kw = keyword["name"]
-                    if (kw == "DNA-binding" or kw == "RNA-binding"):
-                        bp.append("DNA/RNA-binding")
-                    elif (kw == "Nucleotide-binding" or kw == "Metal-binding"):
-                        bp.append(kw)
+    keywords = protein_info["keywords"]
+    for keyword in keywords:
+        kw = keyword["name"]
+        if (kw == "DNA-binding" or kw == "RNA-binding"):
+            bp.append("DNA/RNA-binding")
+        elif (kw == "Nucleotide-binding" or kw == "Metal-binding"):
+            bp.append(kw)
     return bp
 
-fasta_file = "uniprot-download_true_format_fasta_query__28_28proteome_3AUP00005856-2023.06.08-20.05.13.50.fasta"
-json_file = "uniprot-download_true_format_json_query__28_28proteome_3AUP000058566-2023.06.08-21.46.26.53.json"
-motif_file = "data\\motifs_wo_profiles.fasta"
-dpc_file = "data\\dpc.tsv"
-tpc_file = "data\\tpc.tsv"
-paac_file = "data\\paac.tsv"
-cdtc_file = "data\\cdtc.tsv"
-cdtd_file = "data\\cdtd.tsv"
-cdtt_file = "data\\cdtt.tsv"
-ctriad_file = "data\\ctriad.tsv"
-gaac_file = "data\\gaac.tsv"
-moran_file = "data\\moran.tsv"
+#--- Functions: Assign Motifs (Prosite) ---
+def assign_motifs():
+    global proteins
+    with open(motif_file, 'r') as file:
+        data = json.load(file)
+    for id in data:
+        proteins[id]['motifs'] = data[id]
 
-# Load the JSON data
-with open(json_file) as file:
-    data = json.load(file)
-    results = data["results"]
+#--- Functions: Assign iFeature Data (iFeature) ---
+def assign_iFeature_data():
+    global proteins
+    with open(ifeature_file, 'r') as file:
+        features = json.load(file)
+    for feature, ids in features.items():
+        for id, value in ids.items():
+            proteins[id][feature] = value
+                
 
-#organize motifs
-motifs = {}
-for record in SeqIO.parse(motif_file, "fasta"):
-    id = record.id.split("|")[1].strip()
-    seq = str(record.seq)
-    if id not in motifs:
-        motifs[id] = [seq]
-    else:
-        motifs[id].append(seq)
-
-        import csv
-
-#load composition features
-dpcs = create_dictionary_from_tsv(dpc_file)
-tpcs = create_dictionary_from_tsv(tpc_file)
-paacs = create_dictionary_from_tsv(paac_file)
-cdtcs = create_dictionary_from_tsv(cdtc_file)
-cdtds = create_dictionary_from_tsv(cdtt_file)
-cdtts = create_dictionary_from_tsv(cdtd_file)
-ctriads = create_dictionary_from_tsv(ctriad_file)
-gaacs = create_dictionary_from_tsv(gaac_file)
-morans = create_dictionary_from_tsv(moran_file)
-
-proteins = {}
-for record in SeqIO.parse(fasta_file, "fasta"):
+#--- Main: Generate Protein Dictionary ---
+for record in SeqIO.parse(protein_file, "fasta"):
     protein_id = record.id.split("|")[1].strip()
     seq = record.seq
     a_seq = ProteinAnalysis(seq)
@@ -126,48 +100,53 @@ for record in SeqIO.parse(fasta_file, "fasta"):
         "aromaticity": a_seq.aromaticity(),
         "flexibility": a_seq.flexibility(),
         "sec_sruct_frac": a_seq.secondary_structure_fraction(),
-        "go_terms": get_go_terms(protein_id, results),
-        "motifs": motifs[id],
-        "dpc": dpcs[id],
-        "tpc": tpcs[id],
-        "paac": paacs[id],
-        "cdtc": cdtcs[id],
-        "cdtd": cdtds[id],
-        "cdtt": cdtts[id],
-        "ctriad": ctriads[id],
-        "gaac": gaacs[id],
-        "moran": morans[id],
-        "subcell_locations": get_subcellular_locations(protein_id, results),
-        "transmembrane": get_transmembrane(protein_id, results),
-        "binding_preference": get_binding_preference(protein_id, results),
+        "go_terms": [],
+        "motifs": [],
+        "dpc": [],
+        "tpc": [],
+        "paac": [],
+        "cdtc": [],
+        "cdtd": [],
+        "cdtt": [],
+        "ctriad": [],
+        "gaac": [],
+        "moran": [],
+        "subcell_locations": [],
+        "transmembrane": 0,
+        "binding_preference": [],
     }
-
     proteins[protein_id] = protein
 
+assign_annotations()
+assign_motifs()
+assign_iFeature_data()
+
+
+#--- Test: Print Proteins ---
 for protein_id, protein_info in proteins.items():
-    #print(f"Protein ID: {protein_id}")
-    # print(f"Protein Sequence: {protein_info['seq']}")
-    # print(f"Protein Length: {protein_info['length']}")
-    # print(f"Molecular Weight: {protein_info['m_weight']}")
-    # print(f"Instability Index: {protein_info['instab_index']}")
-    # print(f"Isoelectric Point: {protein_info['isoele_point']}")
-    # print(f"Gravy: {protein_info['gravy']}")
-    # print(f"Amino Acid Count: {protein_info['amino_count']}")
-    # print(f"Aromaticity: {protein_info['aromaticity']}")
-    # print(f"Flexibility: {protein_info['flexibility']}")
-    # print(f"Secondary Structure Fraction: {protein_info['sec_sruct_frac']}")
-    # print(f"Gene Ontology Terms: {protein_info['go_terms']}")
-    # print(f"Motifs: {protein_info['motifs']}")
-    # print(f"Dipeptide Composition: {protein_info['dpc']}")
-    # print(f"Tripeptide Composition: {protein_info['tpc']}")
-    # print(f"Pseudo Amino Acid Composition: {protein_info['paac']}")
-    # print(f"Composition: {protein_info['cdtc']}")
-    # print(f"Distribution: {protein_info['cdtd']}")
-    # print(f"Translation: {protein_info['cdtt']}")
-    # print(f"Conjoint Triad: {protein_info['ctriad']}")
-    # print(f"Grouped Amino Acid Composition: {protein_info['gaac']}")
-    # print(f"Moran Autocorrelation: {protein_info['moran']}")
-    # print(f"Subcellular Locations: {protein_info['subcell_locations']}")
-    # print(f"Transmembrane?: {protein_info['transmembrane']}")
-    # print(f"Binding Preference: {protein_info['binding_preference']}")
+    print(f"Protein ID: {protein_id}")
+    print(f"Protein Sequence: {protein_info['seq']}")
+    print(f"Protein Length: {protein_info['length']}")
+    print(f"Molecular Weight: {protein_info['m_weight']}")
+    print(f"Instability Index: {protein_info['instab_index']}")
+    print(f"Isoelectric Point: {protein_info['isoele_point']}")
+    print(f"Gravy: {protein_info['gravy']}")
+    print(f"Amino Acid Count: {protein_info['amino_count']}")
+    print(f"Aromaticity: {protein_info['aromaticity']}")
+    print(f"Flexibility: {protein_info['flexibility']}")
+    print(f"Secondary Structure Fraction: {protein_info['sec_sruct_frac']}")
+    print(f"Gene Ontology Terms: {protein_info['go_terms']}")
+    print(f"Motifs: {protein_info['motifs']}")
+    print(f"Dipeptide Composition: {protein_info['dpc']}")
+    print(f"Tripeptide Composition: {protein_info['tpc']}")
+    print(f"Pseudo Amino Acid Composition: {protein_info['paac']}")
+    print(f"Composition: {protein_info['cdtc']}")
+    print(f"Distribution: {protein_info['cdtd']}")
+    print(f"Translation: {protein_info['cdtt']}")
+    print(f"Conjoint Triad: {protein_info['ctriad']}")
+    print(f"Grouped Amino Acid Composition: {protein_info['gaac']}")
+    print(f"Moran Autocorrelation: {protein_info['moran']}")
+    print(f"Subcellular Locations: {protein_info['subcell_locations']}")
+    print(f"Transmembrane?: {protein_info['transmembrane']}")
+    print(f"Binding Preference: {protein_info['binding_preference']}")
     print()
